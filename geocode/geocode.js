2ff9006f08ce6721e0a24555aed713d1cf572247
f2015537 <divyampatro1997@gmail.com>
2018-11-04 21:43:40 +0530

    This bad boy is ready

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -6,10 +6,8 @@ const geocodeAddress = (address, callback) => {
     url: `http://www.mapquestapi.com/geocoding/v1/address?key=G93bwlH2LGgllgg92mD3LMTPxdky9pAQ&location=${encodedAddress}`,
     json: true
   }, (error, response, body) => {
-    if (error) {
-      callback('Unable to connect to mapquest servers')
-    } else if (!body) {
-      callback('Invalid address')
+    if (error || !body || response.statusCode >= 400) {
+      callback('Either the url is wrong or the location is invalid or the server is down')
     } else {
       callback(undefined, {
         address: body.results[0].providedLocation.location,
